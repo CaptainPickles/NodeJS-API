57a787cb0105eb3fcaf1168666686a88e608bc2e
Lucas Levesque <levesque.lucas@outlook.fr>
2022-05-23 14:26:15 +0200

    feat : added post tasks with mongo

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ mongoose.connect('mongodb://localhost:27017/NodeJsMongo')
 
 const Joi = require('joi');
 
-const schemaJoiTask = Joi.object({
-    id: Joi.number().required(),
-    description: Joi.string().min(2).max(50).required(),
-    faite: Joi.boolean(),
-});
-
 const taskSchema = new mongoose.Schema({
-    id: String,
     description: String,
     faite: Boolean,
 });
@@ -24,16 +17,30 @@ const Task = mongoose.model('Task', taskSchema);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World\n");
+app.get('/', async (req, res) => {
+    const tasks = await Task.find({})
+    res.send(tasks);
 });
 
 app.get('/:id', (req, res) => {
     res.send(req.params.id);
 });
 
-app.post('/', (req, res) => {
-    res.send(req.body);
+app.post('/', async (req, res) => {
+    const schemaCreateTask = Joi.object({
+        description: Joi.string().min(2).max(50).required(),
+        faite: Joi.boolean().required(),
+    });
+    let data
+    try {
+        data = await schemaCreateTask.validateAsync(req.body)
+    } catch (err) {
+        res.status(500).send({ error: err.details[0].message });
+    }
+    if (!data) return
+
+    const createdTask = await Task.create(data)
+    res.send(createdTask);
 });
 app.put('/', (req, res) => {
     res.send('ici traiter PUT')
@@ -42,8 +49,6 @@ app.put('/', (req, res) => {
 app.delete('/', (req, res) => {
     res.send('ici traiter DELETE')
 })
-
-
 
 
 app.listen(3000);
