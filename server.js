6ea87315c67dfc359510284f036d4b39f3eaa42a
Lucas Levesque <levesque.lucas@outlook.fr>
2022-05-23 13:46:20 +0200

    feat : add joi schema

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ mongoose.connect('mongodb://localhost:27017/NodeJsMongo')
     .then(() => console.log('Connected to mongo'))
     .catch((err) => console.log("Not connected to database", err));
 
+const Joi = require('joi');
+
+const schemaJoiTask = Joi.object({
+    id: Joi.number().required(),
+    description: Joi.string().min(2).max(50).required(),
+    faite: Joi.boolean(),
+});
+
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
