9da5d6b31dcd1d8114409b5e22490ce0e247bd29
Lucas Levesque <levesque.lucas@outlook.fr>
2022-05-23 13:40:03 +0200

    git: added conenction to mongodb with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/NodeJsMongo')
+    .then(() => console.log('Connected to mongo'))
+    .catch((err) => console.log("Not connected to database", err));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
